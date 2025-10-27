0b239f859acda810591b1cceb1e7522caab5bcba
jonashoehnes@gmail.com <jonashoehnes@gmail.com>
2025-10-27 13:30:22 -0400

    node env

--- src/Banner.tsx
+++ src/Banner.tsx
@@ -2,21 +2,39 @@ import { useEffect, useState } from "react";
 
 // import pic from "./assets/moyses.jpg";
 export default function Banner({ className }: { className: string }) {
-
-  const [bannerPic, setBannerPic] = useState<string>();
-  
+  // const [bannerPic, setBannerPic] = useState<string>();
+  const [srcUrls, setSrcUrls] = useState<string[]>();
 
   useEffect(() => {
-    import("./assets/moyses.jpg")
-      .then((pic) => setBannerPic(pic.default))
-      .catch((err) => console.error(`Unable to load banner image.\n${err}`));
-  });
+    
+      console.log(">>>" + process.env.NODE_ENV);
+    
+    const fetchSrcUrls = async () => {
+      try {
+        const response = await fetch("/toshalom.org/db/banners.json");
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const result = await response.json();
+        if (process.env.NODE_ENV !== "production") {
+          console.log(JSON.stringify(result) + "DEV MODE");
+        }
+        setSrcUrls(result);
+      } catch (error) {
+        // setError(error);
+      } finally {
+        // setLoading(false);
+      }
+    };
+
+    fetchSrcUrls();
+  }, []);
 
   return (
     <div className={className}>
       <img
         className="border border-red-500 h-100 md:h-120 lg:h-160 w-screen object-cover object-top"
-        src="/toshalom.org/db/images/moyses.jpg"
+        src="/db/images/moyses.jpg"
       ></img>
     </div>
   );
